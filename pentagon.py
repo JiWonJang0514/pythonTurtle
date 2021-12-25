2c8e247248a480dcbcd41a55278d1b02c4f0e874
JiWonJang0514 <82877779+JiWonJang0514@users.noreply.github.com>
2021-12-25 21:56:37 +0900

    Add files via upload

---
+++ pentagon.py
@@ -0,0 +1,18 @@
+import turtle
+t = turtle.Turtle()
+
+t.color('skyblue')
+t.begin_fill()
+
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+
+t.end_fill()
