3cd4f7fa8ed1c60e1ac6946352ef9639ad70d919
JiWonJang0514 <82877779+JiWonJang0514@users.noreply.github.com>
2021-12-26 21:55:10 +0900

    Add files via upload

---
+++ octagon.py
@@ -0,0 +1,24 @@
+import turtle
+t = turtle.Turtle()
+
+t.color('yellowgreen')
+#t.begin_fill()
+
+t.forward(50)
+t.left(45)
+t.forward(50)
+t.left(45)
+t.forward(50)
+t.left(45)
+t.forward(50)
+t.left(45)
+t.forward(50)
+t.left(45)
+t.forward(50)
+t.left(45)
+t.forward(50)
+t.left(45)
+t.forward(50)
+t.left(45)
+
+#t.end_fill()
