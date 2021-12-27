b952e399aa2ab71c37ed2de81722cb78f45badf8
JiWonJang0514 <82877779+JiWonJang0514@users.noreply.github.com>
2021-12-27 23:17:30 +0900

    Add files via upload

---
+++ rect.py
@@ -0,0 +1,16 @@
+import turtle
+t = turtle.Turtle()
+
+t.color('green', 'yellow')
+t.begin_fill()
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+t.end_fill()
